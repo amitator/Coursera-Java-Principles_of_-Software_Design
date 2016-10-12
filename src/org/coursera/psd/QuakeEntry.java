dccef5a081e23a097b614c62d2ab7e2ade790fce
Igor Prus <prus.igor@gmail.com>
2016-10-12 17:17:04 +0300

    removed everything. added some default implementation from Duke University site.

--- src/org/coursera/psd/QuakeEntry.java
+++ src/org/coursera/psd/QuakeEntry.java
@@ -1,19 +1,53 @@
-package org.coursera.psd;
 
-public class QuakeEntry {
+public class QuakeEntry implements Comparable<QuakeEntry> {
     private Location myLocation;
     private String title;
     private double depth;
     private double magnitude;
 
-    public QuakeEntry(double latitude, double longitude, double magnitude, String title, double depth){
-        myLocation = new Location(latitude, longitude);
-        this.depth = depth;
-        this.magnitude = magnitude;
-        this.title = title;
+    public QuakeEntry(double lat, double lon, double mag, 
+    String t, double d) {
+        myLocation = new Location(lat,lon);
+
+        magnitude = mag;
+        title = t;
+        depth = d;
+    }
+
+    public Location getLocation(){
+        return myLocation;
     }
 
     public double getMagnitude(){
-        return this.magnitude;
+        return magnitude;
     }
+
+    public String getInfo(){
+        return title;
+    }
+
+    public double getDepth(){
+        return depth;
+    }
+
+    @Override
+    public int compareTo(QuakeEntry loc) {
+        double difflat = myLocation.getLatitude() - loc.myLocation.getLatitude();
+        if (Math.abs(difflat) < 0.001) {
+            double diff = myLocation.getLongitude() - loc.myLocation.getLongitude();
+            if (diff < 0) return -1;
+            if (diff > 0) return 1;
+            return 0;
+        }
+        if (difflat < 0) return -1;
+        if (difflat > 0) return 1;
+
+        // never reached
+        return 0;
+    }
+
+    public String toString(){
+        return String.format("(%3.2f, %3.2f), mag = %3.2f, depth = %3.2f, title = %s", myLocation.getLatitude(),myLocation.getLongitude(),magnitude,depth,title);
+    }
+
 }
