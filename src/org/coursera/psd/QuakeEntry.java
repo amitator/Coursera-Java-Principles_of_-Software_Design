a9238aec6c9fb745ba9520287a32bd203b8cd60d
Igor Prus <prus.igor@gmail.com>
2016-10-12 15:52:42 +0300

    added QuakeEntry class

---
+++ src/org/coursera/psd/QuakeEntry.java
@@ -0,0 +1,15 @@
+package org.coursera.psd;
+
+public class QuakeEntry {
+    private Location myLocation;
+    private String title;
+    private double depth;
+    private double magnitude;
+
+    public QuakeEntry(double latitude, double longitude, double magnitude, String title, double depth){
+        myLocation = new Location(latitude, longitude);
+        this.depth = depth;
+        this.magnitude = magnitude;
+        this.title = title;
+    }
+}
