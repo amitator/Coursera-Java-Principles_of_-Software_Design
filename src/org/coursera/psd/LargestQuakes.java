ac6b6ea5731d37e0d06f67471cb2e6da42d17d78
Igor Prus <prus.igor@gmail.com>
2016-10-13 22:29:06 +0300

    added indexOfLargest()

--- src/org/coursera/psd/LargestQuakes.java
+++ src/org/coursera/psd/LargestQuakes.java
@@ -12,6 +12,20 @@ public class LargestQuakes {
         System.out.println("findLargestQuakes()");
         System.out.println("read data for "+list.size());
         System.out.println("===============================");
+        int largestIndex = indexOfLargest(list);
+        System.out.println("Largest index is " + largestIndex + " with magnitude "
+                            + list.get(largestIndex).getMagnitude());
+    }
 
+    public int indexOfLargest(ArrayList<QuakeEntry> list){
+        int largestIndex = 0;
+        double maxMagnitude = 0.0d;
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i).getMagnitude() > maxMagnitude){
+                maxMagnitude = list.get(i).getMagnitude();
+                largestIndex = i;
+            }
+        }
+        return largestIndex;
     }
 }
