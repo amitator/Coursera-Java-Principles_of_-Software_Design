793a24750b140399fc0b86fcf4e27776df344eff
Igor Prus <prus.igor@gmail.com>
2016-10-14 16:19:48 +0300

    edited testSort() and tested in main()

---
+++ src/org/coursera/psd/Week_2_Implementing_Selection_Sort/Main.java
@@ -0,0 +1,15 @@
+package org.coursera.psd.Week_2_Implementing_Selection_Sort;
+
+/**
+ * Main. For psvm()
+ *
+ * @author (Igor Prus)
+ * @version (Oct 14/16)
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        QuakeSortInPlace quakeSortInPlace = new QuakeSortInPlace();
+        quakeSortInPlace.testSort();
+    }
+}
--- src/org/coursera/psd/Week_2_Implementing_Selection_Sort/QuakeSortInPlace.java
+++ src/org/coursera/psd/Week_2_Implementing_Selection_Sort/QuakeSortInPlace.java
@@ -33,6 +33,8 @@ public class QuakeSortInPlace {
             in.set(indexOfLargest, qeI);
         }
     }
+
+
    
     public int getSmallestMagnitude(ArrayList<QuakeEntry> quakes, int from) {
         int minIdx = from;
@@ -64,7 +66,8 @@ public class QuakeSortInPlace {
         ArrayList<QuakeEntry> list  = parser.read(source);  
        
         System.out.println("read data for "+list.size()+" quakes");    
-        sortByMagnitude(list);
+//        sortByMagnitude(list);
+        sortByLargestDepth(list);
         for (QuakeEntry qe: list) { 
             System.out.println(qe);
         } 
